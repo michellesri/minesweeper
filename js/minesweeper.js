0b5455417975c1e91de5afd364eb46e28c5f159b
Michelle Sri <michelle.sri@gmail.com>
2018-05-21 19:32:41 -0800

    Added initial models for the cell and board initialization

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,1 +1,24 @@
-console.log("Testing");
+class Minesweeper {
+
+  constructor(dimension, numBombs) {
+    this.dimension = dimension;
+    this.numBombs = numBombs;
+
+    // Initialize the board
+    this.board = [];
+    for (let i = 0; i < dimension; i++) {
+      const innerArr = [];
+      for (let j = 0; j < dimension; j++) {
+        innerArr.push(new Cell());
+      }
+      this.board.push(innerArr);
+    }
+
+    this.board[2][3].isBomb = true
+    this.board[3][4].isRevealed = true
+    this.board[1][5].isFlagged = true
+  }
+}
+
+const minesweeper = new Minesweeper(10, 10);
+console.log(minesweeper.board);
