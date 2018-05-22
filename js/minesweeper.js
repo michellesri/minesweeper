1e4887713ba71f9c6bf165052100be35f41893e7
Michelle Sri <michelle.sri@gmail.com>
2018-05-21 20:37:41 -0800

    Add ability to generate random bombs on board

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -14,8 +14,40 @@
       this.board.push(innerArr);
     }
 
+    console.log(this.generateBombs())
+
     this.board[2][3].isBomb = true
     this.board[3][4].isRevealed = true
     this.board[1][5].isFlagged = true
+  }
+
+  // Array of array, each subarray represents [row, column].
+  generateBombs() {
+    const bombLocations = [];
+    for (var i = 0; i < this.numBombs; i++) {
+      let bomb = this.generateRandomBomb();
+      while (this.doesBombExist(bombLocations, bomb)) {
+        bomb = this.generateRandomBomb();
+      }
+      bombLocations.push(bomb);
+    }
+    return bombLocations;
+  }
+
+  generateRandomBomb() {
+    const rowBomb = Math.floor(Math.random() * this.dimension);
+    const colBomb = Math.floor(Math.random() * this.dimension);
+    return [rowBomb, colBomb];
+  }
+
+  doesBombExist(bombLocations, bomb) {
+    for (let i = 0; i < bombLocations.length; i++) {
+      const match = bomb[0] === bombLocations[i][0] && 
+        bomb[1] === bombLocations[i][1];
+      if (match) {
+        return true;
+      }
+    }
+    return false;
   }
 }
