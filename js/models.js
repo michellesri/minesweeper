1f5525aa5ef5fcac9133140373e9502e8c0d5598
Michelle Sri <michelle.sri@gmail.com>
2018-05-21 19:33:08 -0800

    actually add the models lol

---
+++ js/models.js
@@ -0,0 +1,8 @@
+// The cell represents a single cell in the minesweeper game
+class Cell {
+  constructor() {
+    this.isBomb = false;
+    this.isFlagged = false;
+    this.isRevealed = false;
+  }
+}
